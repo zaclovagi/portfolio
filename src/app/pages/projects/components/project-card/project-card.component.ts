6b24f99ba7a18a2f60a321f6e2d51e0a06951c02
Zac <zaclovagi@gmail.com>
2024-04-30 18:33:46 -0400

    fix background image of project cards

--- src/app/pages/projects/components/project-card/project-card.component.ts
+++ src/app/pages/projects/components/project-card/project-card.component.ts
@@ -5,25 +5,13 @@ import { Component, Input, OnInit } from '@angular/core';
   templateUrl: './project-card.component.html',
   styleUrl: './project-card.component.scss',
 })
-export class ProjectCardComponent implements OnInit {
+export class ProjectCardComponent {
   @Input() title: string = 'Project Title';
   @Input() description: string = 'Project Description';
-  @Input() backgroundImage!: string;
   @Input() techUsed: string[] = [];
 
   @Input() liveUrl?: string;
   @Input() codeUrl?: string;
   @Input() internalUrl?: string;
   @Input() companyName: string = 'Personal Project';
-
-  bgClass = '';
-
-  ngOnInit(): void {
-    console.log(this.backgroundImage);
-    this.bgClass = "bg-[url('" + this.backgroundImage + "')]";
-  }
-
-  openUrl(arg0: string) {
-    throw new Error('Method not implemented.');
-  }
 }
